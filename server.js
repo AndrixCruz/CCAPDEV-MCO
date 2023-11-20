5ce6c146caa8fd3dad60e6a2ae913b3e4f7a514c
AndrixCruz <andrix_thomas_cruz@dlsu.edu.ph>
2023-11-20 20:57:40 +0800

    fix links location

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 
 const app = express();
@@ -16,31 +16,44 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 // MongoDB Connection URL
 const mongoURI = 'mongodb://localhost:27017/'; 
+mongoose.connect(mongoURI, {dbName: 'MCO'});
 
     // Start the server
     app.listen(3000, () => {
       console.log(`YAY`);
-      mongoose.connect(mongoURI, {dbName: 'MCO'});
-      
     // Serve the HTML files
     routes.route('/CCAPDEV-MCO/views/register', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'register.html'));
     });
 
-    routes.route('/CCAPDEV/views/login', (req, res) => {
+    routes.route('/CCAPDEV-MCO/views/login', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'login.html'));
     });
 
-    routes.route('/CCAPDEV/views/profile', (req, res) => {
+    routes.route('/CCAPDEV-MCO/views/profile', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'profile.html'));
     });
 
-    routes.route('/CCAPDEV/views/companyProfile', (req, res) => {
+    routes.route('/CCAPDEV-MCO/views/companyProfile', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'companyProfile.html'));
     });
 
-    routes.route('/CCAPDEV/views/user', (req, res) => {
+    routes.route('/CCAPDEV-MCO/views/user', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', 'user.html'));
     });
   
+    routes.post('/register', async (req, res) => {
+      const { username, email, password, confirmPassword } = req.body;
+      console.log(username, email, password, confirmPassword);
+    
+      // Check if passwords match
+      if (password !== confirmPassword) {
+        return res.status(400).send("Passwords do not match. Please try again.");
+      }
+    
+      const userInfo = {
+        username: username,
+        email: email,
+        password: password,
+      }});
 });
